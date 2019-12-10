31274e1f9fad83ad1e827bdc79f53c532d14a4f8
KatieLG <katie.louise.gardner@gmail.com>
2019-12-10 16:13:49 +0000

    Made 3 polygons and a basic script to sketch out a concept

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+	Rigidbody2D rb;
+	public GameObject[] randoms;
+	float lastTime;
+	float spawnTime = 1f;
+	public float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+		lastTime = Time.time;
+		rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Time.time - lastTime > spawnTime)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				SpawnRandom();
+			}
+			
+
+			lastTime = Time.time;
+		}
+		rb.velocity += Vector2.right * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+		
+    }
+
+	void SpawnRandom()
+	{
+		float posX = Random.Range(transform.position.x - Camera.main.orthographicSize, transform.position.x + Camera.main.orthographicSize);
+		float posY = Random.Range(transform.position.y - Camera.main.orthographicSize/2, transform.position.y - Camera.main.orthographicSize*2);
+
+		int choice = Random.Range(0, randoms.Length);
+		GameObject rando = Instantiate(randoms[choice], new Vector3(posX, posY, 0),Quaternion.identity);
+		float scaleX = Random.Range(transform.localScale.x / 2, transform.localScale.x * 1.5f);
+		float scaleY = Random.Range(transform.localScale.y / 2, transform.localScale.y * 1.5f);
+		rando.transform.localScale = new Vector3(scaleX, scaleY, 1);
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		float grow = 0.01f;
+		if (collision.gameObject.tag == "Respawn")
+		{
+			Destroy(collision.gameObject);
+			transform.localScale += new Vector3(grow,grow,0);
+			Camera.main.orthographicSize += 0.2f;
+		}
+	}
+}
---
+++ Assets/Scripts/SimpleFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour
+{
+	public GameObject target;
+
+	public bool FollowX;
+	public bool FollowY;
+	public bool FollowZ;
+
+	public float offsetX = 0;
+	public float offsetY = 0;
+	public float offsetZ = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		UpdatePosition();
+    }
+
+	void UpdatePosition()
+	{
+		Vector3 myPos = gameObject.transform.position;
+		Vector3 pos = target.transform.position;
+		float newX = FollowX ? (pos.x - offsetX) : myPos.x;
+		float newY = FollowY ? (pos.y - offsetY) : myPos.y;
+		float newZ = FollowZ ? (pos.z - offsetZ) : myPos.z;
+
+		transform.position = new Vector3(newX, newY, newZ);
+	}
+}
